acf162a708a6a6d1fc45149c83b33d5469bd9c3c
Gustavo Mendel <gustavo.mendel.br@gmail.com>
2023-01-24 10:21:53 -0300

    fixing migration bug

--- db/migrate/20221127211532_create_articles.rb
+++ db/migrate/20221127211532_create_articles.rb
@@ -3,7 +3,6 @@ class CreateArticles < ActiveRecord::Migration[7.0]
     create_table :articles do |t|
       t.string :title
       t.text :body
-      t.belongs_to :user, index: { unique: true }, foreign_key: true, null: false
 
       t.timestamps
     end
